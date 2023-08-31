be14dac06a2bcd0e581ec2ea4d80739ea3162a52
TOEYJIRAKIT <jirakid.aa@mail.wu.ac.th>
2023-08-31 15:41:33 +0700

    first commit

---
+++ lib/models/config.dart
@@ -0,0 +1,7 @@
+import 'package:crud/models/users.dart';
+
+class Configure {
+  static const server = "192.168.31.1:3000";
+  static Users login = Users();
+  static List<String> gender = ["None", "Male", "Female"];
+}
